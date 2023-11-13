65ec130a1674d4e8eb4a336da87c32765f638916
José Gabriel Silva <63723069+JoseGabrielSilvaGit@users.noreply.github.com>
2023-11-13 18:28:44 -0300

    Update test.jsx
    Ralei bastante em algo simples. Mas é isso, a vida tem dessas

--- src/routes/test.jsx
+++ src/routes/test.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import './test.css';
 
 function Test() {
-  
+  const artistas = [
+    {name: "Engenheiros do Avaí", image: ""},
+    {name: "Cidade Negra", image: ""},
+    {name: "Capital Inicial", image: ""},
+    {name: "Skank", image: ""},
+    {name: "Paralamas do Sucesso", image: ""},
+    {name: "Lulu Santos", image: ""},
+    {name: "Cazuza", image: ""},
+    {name: "KID Abelha", image: ""},
+    {name: "Biquini Cavadão", image: ""},
+    {name: "O Rappa", image: ""},
+  ];
+
+  const artistasSection = artistas.map((artistUnit, index) => {
+    return (
+      <div id="flex-box" key={index}>
+        <div id='nomeArtistas'>{artistUnit.name}</div>
+        <div id='imagemArtistas'>{artistUnit.image}</div>
+      </div>
+    )
+  });
+
   return (
-    <h1>teste router urrullllllllllllllllllllllllllllllllllllllllllllllllllllllllll</h1>
+    <div id='page2'>    
+      <h1>Artistas</h1>
+      <div class="flex-box" id="artistsBOX">
+          {artistasSection}
+      </div>
+    </div>
+    
   );
 }
 
