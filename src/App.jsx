e73899ffb9103208f08ddd12810d8f4601b6a268
José Gabriel Silva <63723069+JoseGabrielSilvaGit@users.noreply.github.com>
2023-11-13 23:14:39 -0300

    Update App.jsx
    loucura loucura

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,14 @@ function App() {
   return (
     <div>
       <div className="SideBar">
-        <p className="outlet">navbar</p>
-        <Link to="Playlist">Playlist</Link>
-        <p />
-        <Link to="test">teste</Link>
+        <p className="iSpotify" id="iSpotify">iSpotify®</p>
+        <div id="links">
+          <Link to="Playlist" id="playlist" class="myLink">Playlist</Link>
+          <p></p>
+          <Link to="test" id="test1" class="myLink">Artistas</Link>
+          <p></p>
+          <Link to="favorites" id="favoriteSongs" class="myLink"><span id="favoriteSymbol" class="material-symbols-outlined">favorite</span>  Músicas Curtidas</Link>
+        </div>
       </div>
       <div className="Outlet">
         <Outlet />
@@ -18,3 +22,4 @@ function App() {
 }
 
 export default App;
+
