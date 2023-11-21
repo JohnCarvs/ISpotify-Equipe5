9ca6f424a8f88bd2ed932c8a478b81a8f21c5591
José Gabriel Silva <63723069+JoseGabrielSilvaGit@users.noreply.github.com>
2023-11-20 22:07:19 -0300

    Update login.jsx
    tentando fazer essa bagaça andar pra frente (paia)

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -3,8 +3,34 @@ import { useState } from "react";
 import "./login.css";
 import { useEffect } from "react";
 import { Outlet, Link } from "react-router-dom";
+import api from "../services/api";
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const enviarDados = () => {
+    const dados = {
+      email: email,
+      password: senha,
+    };
+
+    fetch('http://localhost:3030/api/users/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(dados),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Sucesso:', data);
+    })
+    .catch(error => {
+      console.error('Erro:', error);
+    });
+  };
   
   return (
     <div>
@@ -12,10 +38,10 @@ function Login() {
       <div id="h1">iSpotify®</div>
       <div>Música para todos.</div>
       <div>
-        <input className="inputs" placeholder="Email" type="email"/><br/>
-        <input className="inputs" placeholder="Senha" type="password"/><br/>
+        <input className="inputs" placeholder="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/><br/>
+        <input className="inputs" placeholder="Senha" type="password" value={senha} onChange={(e) => setSenha(e.target.value)} required/><br/>
        </div>
-      <button id="BotaoEntrar">ENTRAR</button>
+      <button id="BotaoEntrar" onClick={enviarDados}>ENTRAR</button>
       
       <div id="alreadyUser">Não tem uma conta?  <Link to="../Register"  id="link" class="myLink"> INSCREVA-SE</Link> </div>
       
